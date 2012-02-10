938c06b1c4d6c77f85b05112d7312131eafbd4ec
Raffael Schmid <raffael@yux.ch>
2012-02-10 16:32:16 +0100

    ready for 2.7 module structure

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,32 @@
-# init.pp
+# openvpn.pp
 
-import "classes/*.pp"
-import "definitions/*.pp"
+class openvpn {
+    package {
+        "openvpn":
+            ensure => installed;
+    }
+    service {
+        "openvpn":
+            ensure     => running,
+            hasrestart => true,
+            hasstatus  => true,
+            require    => Exec["/etc/default/openvpn concatenation"];
+    }
+    file {
+        "/etc/openvpn":
+            ensure  => directory,
+            require => Package["openvpn"];
+    }
+    file {
+        "/etc/openvpn/keys":
+            ensure  => directory,
+            require => File["/etc/openvpn"];
+    }
+    common::concatfilepart {
+        "00-etc-default-openvpn header":
+            ensure  => present,
+            content => template("openvpn/etc-default-openvpn.erb"),
+            notify  => Service["openvpn"],
+            file    => "/etc/default/openvpn";
+    }
+}
