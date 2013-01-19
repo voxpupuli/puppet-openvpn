dfb1d97f7e85b67a20887faa494bf863a6077067
Raffael Schmid <raffael@yux.ch>
2013-01-19 16:40:19 +0100

    add client configurations

--- vagrant/server.pp
+++ vagrant/server.pp
@@ -4,11 +4,20 @@ node default {
     province     => "ZH",
     city         => "Winterthur",
     organization => "example.org",
-    email        => "root@example.org";
+    email        => "root@example.org",
+    server       => '10.200.200.0 255.255.255.0'
   }
 
   openvpn::client { 'client1':
     server => 'winterthur';
   }
 
+  openvpn::client_specific_config { 'client1':
+    server   => 'winterthur',
+    ifconfig => '10.200.200.100 255.255.255.0'
+  }
+
+  openvpn::client { 'client2':
+    server => 'winterthur';
+  }
 }
