247412d89a11e8850e58405d1eb3b2749181170f
Raffael Schmid <raf@nine.ch>
2013-11-22 16:06:04 +0100

    fix rspec setup, thx to https://gist.github.com/stefanozanella/4190920

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,14 @@
 require 'rubygems'
 require 'puppetlabs_spec_helper/module_spec_helper'
+
+require 'rspec-puppet'
+
+# This will make stdlib functions available when test examples will run
+$:.unshift File.join(File.dirname(__FILE__),  'fixtures', 'modules', 'stdlib', 'lib')
+
+fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
+
+RSpec.configure do |c|
+  c.module_path = File.join(fixture_path, 'modules')
+  c.manifest_dir = File.join(fixture_path, 'manifests')
+end
