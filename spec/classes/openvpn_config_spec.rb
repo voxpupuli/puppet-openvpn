31dde1baf320b4897b917ea66e96ecab0537f968
Bill Weiss <bill@backstopsolutions.com>
2014-05-19 12:23:49 -0500

    Add spec to make sure we don't concat::setup

--- spec/classes/openvpn_config_spec.rb
+++ spec/classes/openvpn_config_spec.rb
@@ -3,10 +3,11 @@ require 'spec_helper'
 describe 'openvpn::config', :type => :class do
   
   it { should create_class('openvpn::config') }
-  
+
   context "on Debian based machines" do
     let (:facts) { { :osfamily => 'Debian', :concat_basedir => '/var/lib/puppet/concat' } }
 
+    if { should not contain_class('concat::setup') }
     it { should contain_concat('/etc/default/openvpn') }
     it { should contain_concat__fragment('openvpn.default.header') }
   end
