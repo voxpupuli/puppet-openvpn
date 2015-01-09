f999323665b28d2cb0cd7caf61b2861025807909
Dominik Schulz <dominik.schulz@gauner.org>
2015-01-09 20:37:17 +0100

    Add tls-cipher options for client and server

--- manifests/client.pp
+++ manifests/client.pp
@@ -59,6 +59,10 @@
 #   String,  Cipher to use for packet encryption
 #   Default: None
 #
+# [*tls_cipher*]
+#   String, TLS Ciphers to use
+#   Default: None
+#
 # [*resolv_retry*]
 #   Integer/String. How many seconds should the openvpn client try to resolve
 #     the server's hostname
@@ -178,6 +182,7 @@ define openvpn::client(
   $verb                 = '3',
   $pam                  = false,
   $cipher               = undef,
+  $tls_cipher           = undef,
   $authuserpass         = false,
   $setenv               = {},
   $setenv_safe          = {},
--- spec/defines/openvpn_client_spec.rb
+++ spec/defines/openvpn_client_spec.rb
@@ -75,6 +75,7 @@ describe 'openvpn::client', :type => :define do
       'persist_key'           => false,
       'persist_tun'           => false,
       'cipher'                => 'BF-CBC',
+      'tls_cipher'            => 'TLS-DHE-RSA-WITH-AES-256-CBC-SHA',
       'port'                  => '123',
       'proto'                 => 'udp',
       'remote_host'           => 'somewhere',
@@ -111,6 +112,7 @@ describe 'openvpn::client', :type => :define do
     it { should contain_file('/etc/openvpn/test_server/download-configs/test_client/test_client.conf').with_content(/^setenv\s+CLIENT_CERT\s+0$/)}
     it { should contain_file('/etc/openvpn/test_server/download-configs/test_client/test_client.conf').with_content(/^setenv_safe\s+FORWARD_COMPATIBLE\s+1$/)}
     it { should contain_file('/etc/openvpn/test_server/download-configs/test_client/test_client.conf').with_content(/^cipher\s+BF-CBC$/)}
+    it { should contain_file('/etc/openvpn/test_server/download-configs/test_client/test_client.conf').with_content(/^tls-cipher\s+TLS-DHE-RSA-WITH-AES-256-CBC-SHA$/)}
     it { should contain_file('/etc/openvpn/test_server/download-configs/test_client/test_client.conf').with_content(/^tls-client$/)}
     it { should contain_file('/etc/openvpn/test_server/download-configs/test_client/test_client.conf').with_content(/^verify-x509-name\s+"test_server"\s+name$/)}
     it { should contain_file('/etc/openvpn/test_server/download-configs/test_client/test_client.conf').with_content(/^sndbuf\s+393216$/)}
