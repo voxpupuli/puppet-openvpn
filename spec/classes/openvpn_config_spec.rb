9f6faf8bd3a8d56b68e57772ad9f1a9730fc00c7
justinc <justinc@buildingiq.com>
2017-02-24 06:33:20 +1100

    Revert to original form

--- spec/classes/openvpn_config_spec.rb
+++ spec/classes/openvpn_config_spec.rb
@@ -1,26 +1,15 @@
 require 'spec_helper'
 
-describe 'openvpn::config' do
-  on_supported_os.each do |os, facts|
-    context "on #{os}" do
-      let(:facts) { facts }
+describe 'openvpn::config', :type => :class do
 
-      it { is_expected.to compile.with_all_deps }
+  it { should create_class('openvpn::config') }
 
-      it { should create_class('openvpn::config') }
-
-      if facts[:osfamily] == 'Debian'
-        it { should contain_concat('/etc/default/openvpn') }
-        it { should contain_concat__fragment('openvpn.default.header') }
-      end
-#  context "on Debian based machines" do
-#    let (:facts) { {
-#      osfamily: 'Debian',
-#      operatingsystem: 'Debian',
-#      operatingsystemrelease: '7',
-#      concat_basedir: '/var/lib/puppet/concat'
-#    } }
-
+  context "on Debian based machines" do
+    let (:facts) { {
+      :osfamily => 'Debian',
+      :operatingsystem => 'Debian',
+      :concat_basedir => '/var/lib/puppet/concat'
+    } }
 
       context "enabled autostart_all" do
         let(:pre_condition) { 'class { "openvpn": autostart_all => true }' }
@@ -34,7 +23,6 @@ describe 'openvpn::config' do
         it { should contain_concat__fragment('openvpn.default.header').with(
           'content' => /^AUTOSTART=""/
         )}
-      end
     end
   end
 end
