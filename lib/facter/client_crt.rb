54c300e918e8ab43bfd3d5a0318928df84f39f56
Kevin Häfeli <kevin@zattoo.com>
2017-05-31 17:23:13 +0200

    add custom facter and resource type for openvpn server

---
+++ lib/facter/client_crt.rb
@@ -0,0 +1,35 @@
+require 'facter'
+
+openvpn_clients = Dir['/etc/openvpn/*/easy-rsa/keys/*.crt'].map { |a| a.gsub(%r{\.crt$}, '').gsub(%r{^.*/}, '') }
+openvpn_ca = Dir.entries('/etc/openvpn').select {|entry| File.directory? File.join('/etc/openvpn',entry) and !(entry =='.' || entry == '..' || entry == 'keys') }
+
+
+Facter.add(:openvpn_clients) do
+  setcode do
+    openvpn_clients.join(',') if openvpn_clients
+  end
+end
+
+Facter.add(:openvpn_ca) do
+  setcode do
+    openvpn_ca.first if openvpn_ca
+  end
+end
+
+openvpn_clients.each do |openvpn_client|
+  Facter.add('openvpn_crt_' + openvpn_client) do
+    setcode do
+      crt = File.read("/etc/openvpn/#{openvpn_ca.first}/easy-rsa/keys/#{openvpn_client}.crt")
+      crt
+    end
+  end
+end
+
+openvpn_clients.each do |openvpn_client|
+  Facter.add('openvpn_key_' + openvpn_client) do
+    setcode do
+      crt = File.read("/etc/openvpn/#{openvpn_ca.first}/easy-rsa/keys/#{openvpn_client}.key")
+      crt
+    end
+  end
+end
