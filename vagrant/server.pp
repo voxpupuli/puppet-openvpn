932e2700a2e35ff7b499e3df529ae5d986468b51
William Yardley <wby@axs.com>
2017-10-05 13:53:46 -0700

    Resolve puppet-lint warnings

--- vagrant/server.pp
+++ vagrant/server.pp
@@ -5,7 +5,7 @@ node default {
     city         => 'Winterthur',
     organization => 'example.org',
     email        => 'root@example.org',
-    server       => '10.200.200.0 255.255.255.0'
+    server       => '10.200.200.0 255.255.255.0',
   }
 
   openvpn::client { 'client1':
@@ -14,7 +14,7 @@ node default {
 
   openvpn::client_specific_config { 'client1':
     server   => 'winterthur',
-    ifconfig => '10.200.200.100 255.255.255.0'
+    ifconfig => '10.200.200.100 255.255.255.0',
   }
 
   openvpn::client { 'client2':
