f3438ed894724967fd3cb35bf52f0902bebc2979
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2020-05-05 18:51:36 +0200

    Unify node setup in acceptance helper

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,35 +1,38 @@
 require 'voxpupuli/acceptance/spec_helper_acceptance'
 
 configure_beaker do |host|
-  # Configure all nodes in nodeset
-  install_server_packages = %(
+  manifest = <<-PUPPET
     if $facts['os']['name'] == 'CentOS' {
       package { 'epel-release':
         ensure => present,
       }
     }
 
-    $package_name = $facts['os']['family'] ? {
+    $netcat_package_name = $facts['os']['family'] ? {
       'Debian' => 'netcat-openbsd',
       'RedHat' => 'nc',
       default  => 'netcat',
     }
-    package { $package_name:
-      ensure => present,
-    }
-  )
-  apply_manifest_on(hosts_as('vpnserver'), install_server_packages, catch_failures: true)
 
-  install_client_packages = %(
-    if $facts['os']['name'] == 'CentOS' {
-      package { 'epel-release':
+    node /^vpnserver\./ {
+      package { $package_name:
         ensure => present,
       }
     }
 
-    package { ['tar','openvpn'] :
-      ensure => present,
+    node /^vpnclienta\./ {
+      package { ['tar','openvpn'] :
+        ensure => present,
+      }
     }
-  )
-  apply_manifest_on(hosts_as('vpnclienta'), install_client_packages, catch_failures: true)
+
+    # CentOS 6 in docker doesn't get a hostname - install all packages
+    node /^localhost\./ {
+      package { ['tar', 'openvpn', $netcat_package_name]:
+        ensure => present,
+      }
+    }
+
+  PUPPET
+  apply_manifest_on(host, manifest, catch_failures: true)
 end
