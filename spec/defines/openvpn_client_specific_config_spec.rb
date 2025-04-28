8c7887c7ad51469d971425249552d61849415eeb
Valantin <valantin89@gmail.com>
2025-04-28 13:28:51 +0200

    Fix Spec Test

--- spec/defines/openvpn_client_specific_config_spec.rb
+++ spec/defines/openvpn_client_specific_config_spec.rb
@@ -2,49 +2,51 @@
 
 require 'spec_helper'
 
-describe 'openvpn::client_specific_config', type: :define do
-  on_supported_os.each do |os, facts|
+describe 'openvpn::client_specific_config' do
+  on_supported_os.each do |os, os_facts|
     context "on #{os}" do
-      let(:pre_condition) do
-        [
-          'openvpn::server { "test_server":
-            country       => "CO",
-            province      => "ST",
-            city          => "Some City",
-            organization  => "example.org",
-            email         => "testemail@example.org"
-          }',
-          'openvpn::client { "test_client":
-            server => "test_server"
-          }'
-        ].join
-      end
       let(:facts) do
-        facts
+        os_facts.merge(
+          easyrsa: '3.0'
+        )
       end
-      let(:title) { 'test_client' }
-      let(:params) do
-        {
-          server: 'test_server'
+      let(:pre_condition) do
+        'openvpn::server { "test_server":
+          country       => "CO",
+          province      => "ST",
+          city          => "Some City",
+          organization  => "example.org",
+          email         => "testemail@example.org"
         }
+        openvpn::client { "test_client":
+          server => "test_server"
+        }'
       end
 
-      server_directory = case facts[:os]['family']
-                         when 'RedHat'
+      let(:title) { 'test_client' }
+
+      server_directory = case os_facts[:os]['family']
+                         when 'Archlinux', 'Debian', 'RedHat'
                            '/etc/openvpn/server'
+                         when 'Solaris'
+                           '/opt/local/etc/openvpn'
+                         when 'FreeBSD'
+                           '/usr/local/etc/openvpn'
                          else
                            '/etc/openvpn'
                          end
 
-      it { is_expected.to compile.with_all_deps }
+      context 'with the minimum parameters' do
+        let(:params) { { server: 'test_server' } }
 
-      it { is_expected.to contain_file("#{server_directory}/test_server/client-configs/test_client") }
+        it { is_expected.to compile.with_all_deps }
 
-      describe 'setting no paramter at all' do
-        it { is_expected.to contain_file("#{server_directory}/test_server/client-configs/test_client").with_content(%r{\A\n\z}) }
+        it {
+          is_expected.to contain_file("#{server_directory}/test_server/client-configs/test_client")
+        }
       end
 
-      describe 'setting all parameters' do
+      context 'with all parameters' do
         let(:params) do
           {
             server: 'test_server',
@@ -59,14 +61,19 @@ describe 'openvpn::client_specific_config', type: :define do
           }
         end
 
-        it { is_expected.to contain_file("#{server_directory}/test_server/client-configs/test_client").with_content(%r{^iroute 10.0.1.0 255.255.255.0$}) }
-        it { is_expected.to contain_file("#{server_directory}/test_server/client-configs/test_client").with_content(%r{^iroute-ipv6 2001:db8:1234::/64$}) }
-        it { is_expected.to contain_file("#{server_directory}/test_server/client-configs/test_client").with_content(%r{^ifconfig-push 10.10.10.2 255.255.255.0$}) }
-        it { is_expected.to contain_file("#{server_directory}/test_server/client-configs/test_client").with_content(%r{^ifconfig-ipv6-push 2001:db8:0:123::2/64 2001:db8:0:123::1$}) }
-        it { is_expected.to contain_file("#{server_directory}/test_server/client-configs/test_client").with_content(%r{^push "dhcp-option DNS 8.8.8.8"$}) }
-        it { is_expected.to contain_file("#{server_directory}/test_server/client-configs/test_client").with_content(%r{^push "redirect-gateway def1"$}) }
-        it { is_expected.to contain_file("#{server_directory}/test_server/client-configs/test_client").with_content(%r{^push "route 10.200.100.0 255.255.255.0 10.10.10.1"$}) }
-        it { is_expected.to contain_file("#{server_directory}/test_server/client-configs/test_client").with_content(%r{^this that$}) }
+        it { is_expected.to compile.with_all_deps }
+
+        it {
+          is_expected.to contain_file("#{server_directory}/test_server/client-configs/test_client").
+            with_content(%r{iroute 10.0.1.0 255.255.255.0}).
+            with_content(%r{iroute-ipv6 2001:db8:1234::/64}).
+            with_content(%r{ifconfig-push 10.10.10.2 255.255.255.0}).
+            with_content(%r{ifconfig-ipv6-push 2001:db8:0:123::2/64 2001:db8:0:123::1}).
+            with_content(%r{route 10.200.100.0 255.255.255.0 10.10.10.1}).
+            with_content(%r{dhcp-option DNS 8.8.8.8}).
+            with_content(%r{this that}).
+            with_content(%r{redirect-gateway})
+        }
       end
     end
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,7 +11,6 @@ require 'voxpupuli/test/spec_helper'
 
 RSpec.configure do |c|
   c.facterdb_string_keys = false
-  c.hiera_config = 'spec/fixtures/hiera/hiera.yaml'
 end
 
 add_mocked_facts!
